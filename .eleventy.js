a4e16260ec6de10dcb5b1eb73f2811878ceaecf4
Kevin Gimbel <kevin@gimbel.dev>
2024-04-28 10:03:26 +0200

    fix: add CNAME to src directory

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ import CleanCSS from 'clean-css';
 
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
 
 	eleventyConfig.addFilter("cssmin", function (code) {
 		return new CleanCSS({}).minify(code).styles;
