ea76ff3b98f36c50474f37b8233239b9990b788c
Kevin Gimbel <kevin@gimbel.dev>
2024-04-28 09:59:28 +0200

    feat: add title and css minification

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
+import CleanCSS from 'clean-css'; 
+
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/");
+
+	eleventyConfig.addFilter("cssmin", function (code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
 
   return {
     dir: {
--- README.md
+++ README.md
@@ -13,3 +13,4 @@ A little website to link to some other websites.
 ## Kudos
 
 - Avatar Image Effect via [cssduotone.com](https://cssduotone.com/)
+- [clean-css](https://github.com/clean-css/clean-css) for CSS Minification
