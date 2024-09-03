affc31f7b588f9f2c93bc21d55b0cbcf511ed13f
Kevin Gimbel <kevin@gimbel.dev>
2024-09-03 08:57:45 +0200

    feat: add talk notes for Container Days 2024

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,12 @@
-import CleanCSS from 'clean-css'; 
+import CleanCSS from "clean-css";
 
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/");
   eleventyConfig.addPassthroughCopy("src/CNAME");
 
-	eleventyConfig.addFilter("cssmin", function (code) {
-		return new CleanCSS({}).minify(code).styles;
-	});
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
 
   return {
     dir: {
